922feba30ad3920b0727609eee6caadca30d7943
FlochieS <tihomirovaslava@gmail.com>
2024-11-09 22:08:28 +0200

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,13 @@
+import colorama
+import inspect
+class Human:
+    pass
+print(help(colorama))
+print(colorama.__name__)
+print(type(colorama))
+print(hasattr(colorama, "index"))
+print(getattr(colorama, "startswith"))
+print(callable(colorama))
+print(isinstance(colorama, Human))
+print(inspect.ismodule(colorama))
+print(inspect.isclass(colorama))
